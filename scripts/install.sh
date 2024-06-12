2de91ea3053e6d52d7bd8270af82e4ecfedc6948
Hypnos <47209391+Jace254@users.noreply.github.com>
2024-06-12 21:02:52 +0300

    chore: better install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,45 +1,56 @@
 #!/bin/bash
 
-# Check if the script is running with sudo
-if [ "$EUID" -ne 0 ]; then
-    echo "This script requires sudo privileges."
-    exit 1
+sudo apt update
+# Check if nvm is installed for the zabbix user
+if [ ! -d "/home/zabbix/.nvm" ]; then
+    echo "nvm is not installed for the zabbix user. Installing nvm..."
+    sudo -u zabbix bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash'
 fi
 
-# Check if nvm is installed
-if ! command -v nvm &> /dev/null; then
-    echo "nvm is not installed. Installing nvm..."
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-    # Activate nvm
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-fi
+# Activate nvm for the zabbix user
+export NVM_DIR="/home/zabbix/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-# Install Node.js and npm using nvm
-echo "-------> Installing Node.js and npm using nvm..."
-nvm install node
+# Install Node.js and npm using nvm for the zabbix user
+echo "-------> Installing Node.js and npm using nvm for the zabbix user..."
+sudo -u zabbix apt install nodejs
+sudo -u zabbix bash -c 'nvm install node'
 
-# Check if pnpm is installed
-if ! command -v pnpm &> /dev/null; then
-    echo "pnpm is not installed. Installing pnpm..."
-    npm install -g pnpm
+# Check if pnpm is installed for the zabbix user
+if ! sudo -u zabbix bash -c 'command -v pnpm &> /dev/null'; then
+    echo "pnpm is not installed for the zabbix user. Installing pnpm..."
+    sudo -u zabbix bash -c 'npm install -g pnpm'
 fi
 
 # Clone the repository
 echo "-------> Cloning the repository"
-git clone https://github.com/Jace254/Citrix-Node.git
+sudo -u zabbix bash -c 'git clone https://github.com/Jace254/Citrix-Node.git'
 
 # Move the cloned repository to the desired location
 echo "-------> Moving the repository to /var/www/html/scripts/citrix-node"
-mkdir /var/www/html/scripts
-mv Citrix-Node /var/www/html/scripts/citrix-node
-cd /var/www/html/scripts/citrix-node && pnpm install
+sudo -u zabbix bash -c 'mkdir -p /var/www/html/scripts'
+sudo -u zabbix bash -c 'mv Citrix-Node /var/www/html/scripts/citrix-node'
+cd /var/www/html/scripts/citrix-node && sudo -u zabbix bash -c 'pnpm install'
 
 # Give execute permission to the script and then add to /usr/local/bin
 echo "-------> Adding get_logon_data to global scope"
-chmod +x /var/www/html/scripts/citrix-node/get_logon_data.sh
-cp /var/www/html/scripts/citrix-node/get_logon_data.sh /usr/local/bin/get_logon_data
+sudo -u zabbix bash -c 'chmod +x /var/www/html/scripts/citrix-node/get_logon_data.sh'
+sudo cp /var/www/html/scripts/citrix-node/get_logon_data.sh /usr/local/bin/get_logon_data
+
+# Check if /usr/lib/zabbix/externalscripts path exists
+if [ ! -d "/usr/lib/zabbix/externalscripts" ]; then
+    echo "Creating /usr/lib/zabbix/externalscripts directory..."
+    mkdir -p /usr/lib/zabbix/externalscripts
+fi
+
+# Copy the script to /usr/lib/zabbix/externalscripts if the path exists
+if [ -d "/usr/lib/zabbix/externalscripts" ]; then
+    echo "Copying get_logon_data script to /usr/lib/zabbix/externalscripts/citrix.sh"
+    cp /var/www/html/scripts/citrix-node/get_logon_data.sh /usr/lib/zabbix/externalscripts/citrix.sh
+else
+    echo "Failed to copy get_logon_data script. /usr/lib/zabbix/externalscripts directory does not exist."
+fi
 
 # Clean up by deleting this script
 echo "-------> Cleaning up"
@@ -47,4 +58,4 @@ rm "$0"
 
 # Run the get_logon_data.sh script with the -h option
 echo "-------> get_logon_data --help"
-get_logon_data -h
+sudo -u zabbix bash -c 'get_logon_data -h'
